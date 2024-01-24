a7a3c78e08064c706fcd76ee98d98f3cd927229e
Prathamesh more <91453437+iamprathameshmore@users.noreply.github.com>
2024-01-25 01:00:07 +0530

    Add files via upload

---
+++ config/site.ts
@@ -0,0 +1,70 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+	name: "Next.js + NextUI",
+	description: "Make beautiful websites regardless of your design experience.",
+	navItems: [
+		{
+			label: "Home",
+			href: "/",
+		},
+    {
+      label: "Products",
+      href: "/docs",
+    },
+    {
+      label: "Pricing",
+      href: "/pricing",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+    },
+    {
+      label: "About",
+      href: "/about",
+    }
+	],
+	navMenuItems: [
+		{
+			label: "Profile",
+			href: "/profile",
+		},
+		{
+			label: "Dashboard",
+			href: "/dashboard",
+		},
+		{
+			label: "Projects",
+			href: "/projects",
+		},
+		{
+			label: "Team",
+			href: "/team",
+		},
+		{
+			label: "Calendar",
+			href: "/calendar",
+		},
+		{
+			label: "Settings",
+			href: "/settings",
+		},
+		{
+			label: "Help & Feedback",
+			href: "/help-feedback",
+		},
+		{
+			label: "Logout",
+			href: "/logout",
+		},
+	],
+	links: {
+		github: "https://github.com/Bugs-and-Glitches",
+		twitter: "https://twitter.com/getnextui",
+		whatsapp: "https://twitter.com/getnextui",
+		docs: "https://chat.whatsapp.com/Ck7nmRUEmEcFWDKNHnvX7y",
+		discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://github.com/iamprathameshmore"
+	},
+};
